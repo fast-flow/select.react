dc8800e81ae8a4632a40b5fc7c1aa153a34ad308
Nimo <nimo.jser@gmail.com>
2017-02-14 15:02:08 +0800

    open close menu

--- example/basic.demo.js
+++ example/basic.demo.js
@@ -1,7 +1,6 @@
 var React = require('react')
 var Select = require('select.react').default
 var Option = require('select.react').Option
-var Align = require('rc-align')
 var App = React.createClass({
     getInitialState: function () {
         return {
@@ -15,28 +14,6 @@ var App = React.createClass({
         var self = this
         return (
             <div>
-
-                <div ref="demo" style={{width: 100, border: "1px solid red"}} >
-                    ABCD
-                </div>
-                <Align
-                   target={this.getTarget}
-                   monitorWindowResize={true}
-                   align={
-                       {points: ['tc', 'bc']}
-                   }
-                 >
-                   <div
-                     style={{
-                       position: 'absolute',
-                       width: 50,
-                       height: 50,
-                       background: 'yellow',
-                     }}
-                   >
-                     source
-                   </div>
-                 </Align>
                  <Select value={self.state.value} onChange={function (value) {
                          self.setState({
                              value: value
