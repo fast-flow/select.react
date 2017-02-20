2acb859764da958d9e6cad0fca62c0cd1c7a2415
Nimo <nimo.jser@gmail.com>
2017-02-20 15:14:33 +0800

    fast-flow/react-component auto-generated commit

---
+++ 0.2.1/example/basic.demo.js
@@ -0,0 +1,74 @@
+var React = require('react')
+var Select = require('select.react').default
+var Option = require('select.react').Option
+var App = React.createClass({
+    getInitialState: function () {
+        return {
+            value: "a",
+            multipleValue: ["b", "c", "d"]
+        }
+    },
+    render: function () {
+        var self = this
+        return (
+            <div>
+                empty:
+                 <Select
+                     menuStyle={{width: 100}}
+                     arrows={(
+                         <div className="r-select-menu-arrows" >{/* r-select-menu-arrows--center */}</div>
+                     )}
+                     empty={(<em>无数据</em>)}
+                      >
+                 </Select>
+                 <hr />
+                 <Select value={self.state.value} placeholder="请选择一项" onChange={function (value) {
+                         self.setState({
+                             value: value
+                         })
+                     }}
+                 >
+                     <Option value="">请选择</Option>
+                     <Option value="a" >nimo</Option>
+                     <Option value="b" view="NICO" >nico</Option>
+                     <Option value="c" >judy</Option>
+                     <Option value="d" className="myOption" >tim</Option>
+                     <Option value="4194291487128419429" disabled className="titleOption" >title</Option>
+                     <Option value="g" >tim</Option>
+                     <Option value="h" >hans</Option>
+                     <Option value="i" >orly</Option>
+                     <Option value="j" >tim</Option>
+                     <Option value="k" >etude</Option>
+                     <Option value="l" >liza</Option>
+                 </Select>
+                 <hr />
+                     <Select value={self.state.multipleValue} multiple onChange={function (value) {
+                             self.setState({
+                                 multipleValue: value
+                             })
+                         }}
+                         header={(
+                             <div>
+                                 <span>选择多项</span>
+                                 <span className="r-select-menu-header-close" data-r-select-close="true" ></span>
+                             </div>
+                         )}
+                      >
+                         <Option value="a" >nimo</Option>
+                         <Option value="b" >nico</Option>
+                         <Option value="c" view="JUDY" >judy</Option>
+                         <Option value="d" view={(<em>Tim</em>)} >tim</Option>
+                         <Option value="e" >jack</Option>
+                         <Option value="f" >title</Option>
+                         <Option value="g" >ppk</Option>
+                         <Option value="h" >hans</Option>
+                         <Option value="i" >orly</Option>
+                         <Option value="j" >efil</Option>
+                         <Option value="k" >etude</Option>
+                         <Option value="l" >liza</Option>
+                     </Select>
+             </div>
+        )
+    }
+})
+module.exports = App
