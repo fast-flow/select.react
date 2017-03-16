d7ecb992fac7b2ff6b292874718f1a6d4114d1c0
Nimo <nimo.jser@gmail.com>
2017-03-16 13:25:34 +0800

    del className

--- __test__/basic.test.js
+++ __test__/basic.test.js
@@ -1,8 +1,8 @@
 import { Component } from "react"
 import Select from "../lib/index"
 import { shallow, mount, render } from "enzyme"
-
-it('className', function () {
-    const app = mount(<Select className="myclass" />)
-    expect(app.find('.myclass').length).toEqual(1)
-})
+//
+// it('className', function () {
+//     const app = mount(<Select className="myclass" />)
+//     expect(app.find('.myclass').length).toEqual(1)
+// })
