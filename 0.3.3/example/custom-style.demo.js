4d0fb33f84e3fa2ac42b1cf9ba30152aecd198bd
Nimo <nimo.jser@gmail.com>
2017-03-17 10:04:38 +0800

    fast-flow/react-component auto-generated commit

---
+++ 0.3.3/example/custom-style.demo.js
@@ -0,0 +1,10 @@
+var React = require('react')
+var Select = require('select.react').default
+var App = React.createClass({
+    render: function () {
+        return (
+            <Select className="alert--info" >basic</Select>
+        )
+    }
+})
+module.exports = App
