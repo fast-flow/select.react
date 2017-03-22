47f5203c66271223a048004ac09646555d6eba7f
Nimo <nimo.jser@gmail.com>
2017-03-22 14:10:51 +0800

    fast-flow/react-component auto-generated commit

---
+++ 0.4.1/lib/Option.js
@@ -0,0 +1,47 @@
+import { Component, PropTypes } from "react"
+import classNames from "classnames"
+class Option extends Component {
+    onClick = (e) => {
+        const self = this
+        if (self.props.disabled) {
+            return
+        }
+        if (self.context.props.multiple) {
+            self.context.props.multipleChange(self.props.value, e)
+        }
+        else {
+            self.context.props.singleChanged(self.props.value, e)
+        }
+    }
+    render() {
+        const self = this
+        let props = self.context.props || {}
+        const pcls = props.prefixClassName
+        let selected
+        if (props.multiple) {
+            selected = (self.context.props.value.indexOf(self.props.value) !== -1)
+        }
+        else {
+            selected = (self.props.value.length !== 0 && self.props.value === self.context.props.value)
+        }
+        return (
+            <div className={classNames({
+                    [self.props.className]: true,
+                    [`${pcls}-menu-list-item`]: true,
+                    [`${pcls}-menu-list-item--disabled`]: self.props.disabled,
+                    [`${pcls}-menu-list-item--selected`]: selected
+                })} onClick={self.onClick}>{this.props.children}</div>
+        )
+    }
+}
+Option.defaultProps = {
+    className: '',
+    value: ''
+}
+Option.propTypes = {
+    className: PropTypes.string
+}
+Option.contextTypes = {
+    props: PropTypes.object
+}
+export default Option
